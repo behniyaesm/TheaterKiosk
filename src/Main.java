115b14e392740bf5c36c1ba4315c1827c91f8e21
behniyaesm <behniyaesm@gmail.com>
2024-09-28 12:40:35 -0400

    Completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
+import java.util.Scanner;
+public class Main
+{
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner console = new Scanner(System.in);
+        int age = 0;
+
+        System.out.println("Enter your age: ");
+        age = console.nextInt();
+
+        if (age >= 21) {
+            System.out.println("You get a wristband!");
+        }
     }
 }
